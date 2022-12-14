86040e6d71b1c16aeffb20a48d330e01c16d8b69
tony555t <tonytullah764@gmail.com>
2022-12-14 15:09:45 +0300

    npoint

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
-
-//const url1 = 'http://localhost:3000/films/1' 
+// first movie fetch url
+const url1 = 'http://localhost:3000/films/1' 
+// all movies fetch url
 let url = 'http://localhost:3000/films'
 
 document.addEventListener('DOMContentLoaded', ()=>{
@@ -9,20 +10,20 @@ document.addEventListener('DOMContentLoaded', ()=>{
     // get first movie details
     const fetchData1 = () => {
     fetch(url1).then(res => res.json())
-    .then(movieOne => {
+    .then(firstMovie => {
         const movie1 =  document.getElementById('poster')
         console.log(movie1)
-        movie1.src = fMovie.poster
+        movie1.src = firstMovie.poster
         const movie1Title = document.querySelector('#title');
-        movie1Title.textContent = movieOne.title;
+        movie1Title.textContent = firstMovie.title;
         const movie1Time = document.querySelector('#runtime');
-        movie1Time.textContent = `${movieone.runtime} minutes`;
+        movie1Time.textContent = `${firstMovie.runtime} minutes`;
         const movie1Description = document.querySelector('#description');
-        movie1Description.textContent = movieOne.description;
+        movie1Description.textContent = firstMovie.description;
         const showTime = document.querySelector('#showtime')
-        showTime.textContent = movieOne.showtime;
-        const tickets  = document.querySelector('#ticketnum')
-        tickets.textContent = movieOne.capacity - MovieOne.tickets_sold;
+        showTime.textContent = firstMovie.showtime;
+        const tickets  = document.querySelector('#available-ticket')
+        tickets.textContent = firstMovie.capacity - firstMovie.tickets_sold;
         const btn = document.getElementById('buy-ticket')
         btn.textContent = 'buy movie'
 
@@ -31,7 +32,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
     const navBar = document.getElementById('movies-list')
 
-    //fetch function
+    //Create fetch function
     const fetchAllMovies = url => {
         fetch(url).then(res => res.json())
         .then(movies => {
@@ -41,7 +42,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
         })
     }
 
-    // movies lists 
+    // display movies lists in the menu 
     const displayAllMovies = movie => {
     
         const li = document.createElement('li')
@@ -51,14 +52,14 @@ document.addEventListener('DOMContentLoaded', ()=>{
         displayMovieDetails()
     }
 
-    
+    // click to dsplay movie details
     const displayMovieDetails = () =>{
         let children = navBar.children
-    
+        // console.log(children)
 
         for(let i=0; i<children.length; i++){
             let child = children[i]
-        
+            // console.log(child)
 
             child.addEventListener('click',() => {
                 fetch(`${url}/${i+1}`)
@@ -72,7 +73,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
             })
         }
     }
-    // details card
+    // create movie details card
     function setUpMovieDetails(childMovie){
         const preview = document.getElementById('poster')
         preview.src = childMovie.poster;
@@ -81,20 +82,20 @@ document.addEventListener('DOMContentLoaded', ()=>{
         movieTitle.textContent = childMovie.title;
         const movieTime = document.querySelector('#runtime');
         movieTime.textContent = `${childMovie.runtime} minutes`;
-        const movieDescription = document.querySelector('#filminfo');
+        const movieDescription = document.querySelector('#description');
         movieDescription.textContent = childMovie.description;
-        const showTime = document.querySelector('showtime')
+        const showTime = document.querySelector('#showtime')
         showTime.textContent = childMovie.showtime;
-        const tickets  = document.querySelector('#ticketnum')
+        const tickets  = document.querySelector('#available-ticket')
         tickets.textContent = childMovie.capacity - childMovie.tickets_sold;
     }
     const btn = document.getElementById('buy-ticket')
 
             btn.addEventListener('click', function(e){
-                let availableTicket = document.querySelector('#ticketnum').textContent
+                let availableTicket = document.querySelector('#available-ticket').textContent
                 e.preventDefault()
                 if(availableTicket > 0){
-                    document.querySelector('#ticketnum').textContent  = availableTicket-1
+                    document.querySelector('#available-ticket').textContent  = availableTicket-1
                     
                 }
                 else if(parseInt(availableTicket, 10)===0){
